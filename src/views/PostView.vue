1b09557f61fe924d1530fdf44b9a3db52956a7f7
Mauro Perez <maurop123@gmail.com>
2022-11-23 20:38:56 -0700

    snap picture and copy to canvas. cancel function too

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,7 +1,9 @@
 <script setup>
-  import { onMounted, ref } from 'vue'
+  import { onMounted, reactive, ref } from 'vue'
 
+  const state = reactive({ pictureSnapped: false })
   const video = ref(null)
+  const canvas = ref(null)
 
   onMounted(() => {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -10,8 +12,23 @@
       })
     }
   })
+
+  function snap() {
+    console.log('canvas', canvas)
+    const context = canvas.value.getContext('2d')
+    console.log('context', context)
+    context.drawImage(video.value, 0, 0, 640, 480)
+    state.pictureSnapped = true
+  }
+
+  function cancel() {
+    state.pictureSnapped = false
+  }
 </script>
 
 <template>
-  <video ref="video" width="640" height="480" autoplay />
+  <video ref="video" v-show="!state.pictureSnapped" width="640" height="480" autoplay />
+  <canvas ref="canvas" v-show="state.pictureSnapped" width="640" height="480" />
+  <button @click="snap" v-if="!state.pictureSnapped">Snap</button>
+  <button @click="cancel" v-if="state.pictureSnapped">Cancel</button>
 </template>
